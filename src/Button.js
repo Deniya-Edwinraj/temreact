7c7d2fea3c24bdc1f8bba33aecd1a9c1b2d7cb35
tharshagini <tharshaginitharsha@gmail.com>
2023-12-29 11:34:46 +0530

    done

---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Button() {
+  return (
+    <div className="Button">
+     <button>Get Started</button>
+    <Text></Text>
+    </div>
+  );
+}
+function Text() {
+    return (
+      <div className="Text">
+       <p>Get Started</p>
+      </div>
+    );
+  }
+
+export default Button;
